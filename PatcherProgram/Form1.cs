68e759217fe97a4068402ee42acfff20b25ae0eb
Terrance Codd <tcodd86@msn.com>
2013-10-25 13:52:54 -0400

    Simplified some code in the calc Raman shift button.

--- PatcherProgram/Form1.cs
+++ PatcherProgram/Form1.cs
@@ -20,20 +20,13 @@ namespace WindowsFormsApplication1
         //declare variables
         public string[] CRDSFileArray = {};
         public string[] WLMFileArray = {};
-        public string[] CRDSNewLine;
-        public string[] WLMNewLine;
         public string[] Empty = {};
         public string[,] Empty2 = {};
-        public string[] CRDSSeg;
-        public string[] WLMSeg;
-        //public string[,] PatchSeg;
-        //public decimal[,] PatchSeg;
         public string filePath;
         public decimal valueToAdd = 0M;
         public decimal ramanShift = 4155.25000M;
         public decimal pfreq = 0M;
         public int numberOfColumnsCRDS = 6;
-        //public int f = 0;
         public int numberOfShots = 4;
         public int onCol;
         public int offCol;
@@ -128,147 +121,146 @@ namespace WindowsFormsApplication1
             if (WLMFileArray.Length == 0 || CRDSFileArray.Length == 0)
             {
                 MessageBox.Show("Please make sure you have loaded both a CRDS and WLM file.", "Error!");
+                return;
             }
+
+            //this replaces the last timing point with a 500 so the last segment is read
+            int endOfFileMarker = numberOfShots * 400;
+            string endOfFileMarkerS = Convert.ToString(endOfFileMarker);
+            WLMFileArray[WLMFileArray.Length - 2] = endOfFileMarkerS;
+
+            decimal calFreqAdj;
+
+            //this adds the Value to Add and subtracts the Raman Shift
+            //this is for user input RamanShift values
+            if (!CalcRamanShift.Checked)
+            {
+                for (int var = 1; var < WLMFileArray.Length; var += 5)
+                {
+                    calFreqAdj = Convert.ToDecimal(WLMFileArray[var]) + valueToAdd - ramanShift;
+                    WLMFileArray[var] = Convert.ToString(calFreqAdj);
+                }//end for loop for adding and subtracting values
+            }
+            //this is for User input T and P values to calculate the RamanShift
             else
             {
-                //this replaces the last timing point with a 500 so the last segment is read
-                int endOfFileMarker = numberOfShots * 400;
-                string endOfFileMarkerS = Convert.ToString(endOfFileMarker);
-                WLMFileArray[WLMFileArray.Length - 2] = endOfFileMarkerS;
-
-                decimal calFreqAdj;
-
-                //this adds the Value to Add and subtracts the Raman Shift
-                //this is for user input RamanShift values
-                if (!CalcRamanShift.Checked)
+                decimal RS = RamanShift();
+                for (int var = 1; var < WLMFileArray.Length; var += 5)
                 {
-                    for (int var = 1; var < WLMFileArray.Length; var += 5)
+                    calFreqAdj = Convert.ToDecimal(WLMFileArray[var]) + valueToAdd - RS;
+                    WLMFileArray[var] = Convert.ToString(calFreqAdj);
+                }//end for loop for adding and subtracting values
+            }
+
+            //creates a list of segments and then initializes them all
+            var SegList = new List<Segment>();
+            bool temp = true;
+            int whichSeg = 0;
+            while (temp)
+            {
+                SegList.Add(new Segment(valueToAdd, ramanShift, numberOfShots, checkBox1.Checked, cutWLMpoints.Checked, (int)numberOfColumns.Value));
+                temp = SegList.Last().patchSegs(CRDSFileArray, WLMFileArray, whichSeg);
+                whichSeg++;
+            }
+            bubbleSort(ref SegList);
+
+            var allData = new List<Decimal[]>();
+
+            //conditional statement to patch segments or not
+            if (checkBox1.Checked)
+            {
+                for (int i = 0; i < SegList.Count; i++)
+                {
+                    for (int j = 0; j < SegList[i].patchSeg.GetLength(0); j++)
                     {
-                        calFreqAdj = Convert.ToDecimal(WLMFileArray[var]) + valueToAdd - ramanShift;
-                        WLMFileArray[var] = Convert.ToString(calFreqAdj);
-                    }//end for loop for adding and subtracting values
-                }
-                //this is for User input T and P values to calculate the RamanShift
-                else
-                {
-                    decimal RS = RamanShift();
-                    for (int var = 1; var < WLMFileArray.Length; var += 5)
-                    {
-                        calFreqAdj = Convert.ToDecimal(WLMFileArray[var]) + valueToAdd - RS;
-                        WLMFileArray[var] = Convert.ToString(calFreqAdj);
-                    }//end for loop for adding and subtracting values
-                }
-
-                //creates a list of segments and then initializes them all
-                var SegList = new List<Segment>();
-                bool temp = true;
-                int whichSeg = 0;
-                while (temp)
-                {
-                    SegList.Add(new Segment(valueToAdd, ramanShift, numberOfShots, checkBox1.Checked, cutWLMpoints.Checked, (int)numberOfColumns.Value));
-                    temp = SegList.Last().patchSegs(CRDSFileArray, WLMFileArray, whichSeg);
-                    whichSeg++;
-                }
-                bubbleSort(ref SegList);
-
-                var allData = new List<Decimal[]>();
-
-                //conditional statement to patch segments or not
-                if (checkBox1.Checked)
-                {
-                    for (int i = 0; i < SegList.Count; i++)
-                    {
-                        for (int j = 0; j < SegList[i].patchSeg.GetLength(0); j++)
-                        {
-                            if (allData.Count != 0 && SegList[i].patchSeg[j, 0] < allData[allData.Count - 1][0])
-                            {
-                                continue;
-                            }
-                            decimal[] tempMat = new decimal[1 + (int)numberOfColumns.Value + 1];
-                            for (int k = 0; k < tempMat.Length; k++)
-                            {
-                                tempMat[k] = SegList[i].patchSeg[j, k];
-                            }
-                            allData.Add(tempMat);
-                        }
-                    }
-                }//end checkBox if condition
-                else
-                {
-                    for (int i = 0; i < SegList.Count; i++)
-                    {
-                        for (int j = 0; j < SegList[i].patchSeg.GetLength(0); j++)
-                        {
-                            decimal[] tempMat = new decimal[1 + (int)numberOfColumns.Value + 1];
-                            for (int k = 0; k < tempMat.Length; k++)
-                            {
-                                tempMat[k] = SegList[i].patchSeg[j, k];
-                            }
-                            allData.Add(tempMat);
-                        }
-                    }
-                }
-
-
-
-                for (int rowIndex = 0; rowIndex < allData.Count; rowIndex++)
-                {
-                    if (cutWLMpoints.Checked)
-                    {
-                        if (allData[rowIndex][0] < 0M)
+                        if (allData.Count != 0 && SegList[i].patchSeg[j, 0] < allData[allData.Count - 1][0])
                         {
                             continue;
                         }
-                    }
-                    StringBuilder line = new StringBuilder();
-                    if (XYOnly.Checked)
-                    {
-                        line.Append(Convert.ToString(allData[rowIndex][0])).Append("\t");
-                        line.Append(Convert.ToString(allData[rowIndex][numberOfColumnsCRDS + 1]));
-                    }
-                    else
-                    {
-                        for (int colIndex = 0; colIndex < 1 + numberOfColumnsCRDS + 1; colIndex++)
+                        decimal[] tempMat = new decimal[1 + (int)numberOfColumns.Value + 1];
+                        for (int k = 0; k < tempMat.Length; k++)
                         {
-                            line.Append(Convert.ToString(allData[rowIndex][colIndex])).Append("\t");
-                        }//inner for
+                            tempMat[k] = SegList[i].patchSeg[j, k];
+                        }
+                        allData.Add(tempMat);
                     }
-                    linesToWrite.Add(line.ToString());
-                }//end line writer for
-
-
-                if (String.IsNullOrEmpty(filePath))
+                }
+            }//end checkBox if condition
+            else
+            {
+                for (int i = 0; i < SegList.Count; i++)
                 {
-                    savePatched.InitialDirectory = "V:\\tcodd\\HRCRDS Data\\NO3";//V:\\tcodd\\HRCRDS Data
-                }//end if
+                    for (int j = 0; j < SegList[i].patchSeg.GetLength(0); j++)
+                    {
+                        decimal[] tempMat = new decimal[1 + (int)numberOfColumns.Value + 1];
+                        for (int k = 0; k < tempMat.Length; k++)
+                        {
+                            tempMat[k] = SegList[i].patchSeg[j, k];
+                        }
+                        allData.Add(tempMat);
+                    }
+                }
+            }
+
+
+
+            for (int rowIndex = 0; rowIndex < allData.Count; rowIndex++)
+            {
+                if (cutWLMpoints.Checked)
+                {
+                    if (allData[rowIndex][0] < 0M)
+                    {
+                        continue;
+                    }
+                }
+                StringBuilder line = new StringBuilder();
+                if (XYOnly.Checked)
+                {
+                    line.Append(Convert.ToString(allData[rowIndex][0])).Append("\t");
+                    line.Append(Convert.ToString(allData[rowIndex][numberOfColumnsCRDS + 1]));
+                }
                 else
                 {
-                    savePatched.InitialDirectory = filePath;
-                }//end else
-                savePatched.OverwritePrompt = true;
-                savePatched.CreatePrompt = false;
-                savePatched.FileName = "";
-                savePatched.Filter = "Text|*.txt|All Files|*.*";
-                savePatched.ShowDialog();
-                filePath = savePatched.FileName;
+                    for (int colIndex = 0; colIndex < 1 + numberOfColumnsCRDS + 1; colIndex++)
+                    {
+                        line.Append(Convert.ToString(allData[rowIndex][colIndex])).Append("\t");
+                    }//inner for
+                }
+                linesToWrite.Add(line.ToString());
+            }//end line writer for
 
-                File.WriteAllLines(filePath, linesToWrite, Encoding.ASCII);
 
-                Segment work = new Segment(valueToAdd, ramanShift, numberOfShots, checkBox1.Checked, cutWLMpoints.Checked, (int)numberOfColumns.Value);
-                work.patchSegs(CRDSFileArray, WLMFileArray, 0);
-
-                //The following resets all of the variables after each save
-                CRDSFileArray = Empty;
-                WLMFileArray = Empty;
-                linesToWrite.Clear();
-                pfreq = 0M;
-                //Take care of static variables for Segment objects
-                Segment.nnprevW = 5;
-                Segment.nprev = 0;
-                Segment.wLF = 8;
-                Segment.pfreq = 0M;
-                Segment.seg = -1;
+            if (String.IsNullOrEmpty(filePath))
+            {
+                savePatched.InitialDirectory = "V:\\tcodd\\HRCRDS Data\\NO3";//V:\\tcodd\\HRCRDS Data
+            }//end if
+            else
+            {
+                savePatched.InitialDirectory = filePath;
             }//end else
+            savePatched.OverwritePrompt = true;
+            savePatched.CreatePrompt = false;
+            savePatched.FileName = "";
+            savePatched.Filter = "Text|*.txt|All Files|*.*";
+            savePatched.ShowDialog();
+            filePath = savePatched.FileName;
+
+            File.WriteAllLines(filePath, linesToWrite, Encoding.ASCII);
+
+            Segment work = new Segment(valueToAdd, ramanShift, numberOfShots, checkBox1.Checked, cutWLMpoints.Checked, (int)numberOfColumns.Value);
+            work.patchSegs(CRDSFileArray, WLMFileArray, 0);
+
+            //The following resets all of the variables after each save
+            CRDSFileArray = Empty;
+            WLMFileArray = Empty;
+            linesToWrite.Clear();
+            pfreq = 0M;
+            //Take care of static variables for Segment objects
+            Segment.nnprevW = 5;
+            Segment.nprev = 0;
+            Segment.wLF = 8;
+            Segment.pfreq = 0M;
+            Segment.seg = -1;
         }//end patchButtonClick
 
         private static void bubbleSort(ref List<Segment> user)
@@ -295,24 +287,17 @@ namespace WindowsFormsApplication1
 
         private void checkBox2_CheckedChanged(object sender, EventArgs e)
         {
+            bool temp = false;
             if (CalcRamanShift.Checked)
             {
-                TempLabel.Enabled = true;
-                PressureLabel.Enabled = true;
-                TemperatureUpDown.Enabled = true;
-                PressureUpDown.Enabled = true;
-                RegRamanlabel.Enabled = false;
-                ramanShiftUpDown.Enabled = false;
+                temp = true;
             }
-            else
-            {
-                TempLabel.Enabled = false;
-                PressureLabel.Enabled = false;
-                TemperatureUpDown.Enabled = false;
-                PressureUpDown.Enabled = false;
-                RegRamanlabel.Enabled = true;
-                ramanShiftUpDown.Enabled = true;
-            }
+            TempLabel.Enabled = temp;
+            PressureLabel.Enabled = temp;
+            TemperatureUpDown.Enabled = temp;
+            PressureUpDown.Enabled = temp;
+            RegRamanlabel.Enabled = !temp;
+            ramanShiftUpDown.Enabled = !temp;
         }//end method bublleSort
 
         /// <summary>
